746822b522505666003cc8b1e1a6e69374a0a6d0
Velina Chakarova <65416281+velichakarova@users.noreply.github.com>
2021-02-06 14:48:32 +0000

    add router middlewere

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 
 const config = require('./config/config.js');
-const handlebars = require('express-handlebars');
+const expressConfig = require('./config/express');
+const routes = require('./routes')
 const app = express();
 
-app.engine('hbs', handlebars({
-    extname:'hbs'
-}));
-app.set('view engine', 'hbs')
-app.use(express.static(`static`))
+expressConfig(app);
+app.use(routes);
 
-app.get('/',(req,res)=>{
-    res.render('home',{layout:false})
-});
 app.listen(config.PORT, ()=>
 console.log(`Server is running on port ${config.PORT}...`));
